c7f11850c28278bd384da4c17ad086b72da66b31
tonghongbo <tonghongbo1991@163.com>
2019-09-01 17:02:48 +0800

    1、@EnableEurekaServer解释 2、@EnableDiscoveryClient解释

--- eureka-server/src/main/java/com/mango/tong/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/mango/tong/EurekaServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+//标记为注册中心
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
--- springcloud-hello/src/main/java/com/mango/tong/hello/SpringcloudHelloApplication.java
+++ springcloud-hello/src/main/java/com/mango/tong/hello/SpringcloudHelloApplication.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//@EnableDiscoveryClient和@EnableEurekaClient
+//共同点：都是能够让注册中心能够发现，扫描到改服务。
+//不同点：@EnableEurekaClient只适用于Eureka作为注册中心，@EnableDiscoveryClient可以是其他注册中心。
+//从Spring Cloud Edgware开始，@EnableDiscoveryClient或@EnableEurekaClient可省略。只需加上相关依赖，并进行相应配置，即可将微服务注册到服务发现组件上。
 public class SpringcloudHelloApplication {
 
     public static void main(String[] args) {
