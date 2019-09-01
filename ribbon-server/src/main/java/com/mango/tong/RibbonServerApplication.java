eb59577dba927c500eb41d21da16f339eeeb1f94
tonghongbo <tonghongbo1991@163.com>
2019-09-01 18:19:49 +0800

    ribbon解释

--- ribbon-server/src/main/java/com/mango/tong/RibbonServerApplication.java
+++ ribbon-server/src/main/java/com/mango/tong/RibbonServerApplication.java
@@ -6,6 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+//ribbon是一个基于http和tcp的客户端负载均衡器，它可以在通过客户端中配置的ribbonServerList服务端列表去轮询访问以达到均衡负载的作用。
+//当ribbon与eureka联合使用时，ribbon的服务实例清单ribbonServerList会被DiscoveryEnabledNIWSServerList重写，
+// 扩展成从eureka注册中心中获取服务端列表。同时它也会用NIWSDiscoveryPing来取代IPing，它将职责委托给eureka来确定服务端是否已经启动。
 public class RibbonServerApplication {
 
     public static void main(String[] args) {
