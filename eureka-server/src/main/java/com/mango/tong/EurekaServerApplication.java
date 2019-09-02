44fc04c4823adbdd38d4eaa306fd5a4d650fde82
tonghongbo <935092850@qq.com>
2019-09-02 10:09:53 +0800

    1、unicode转中文 2、注释规范

--- eureka-server/src/main/java/com/mango/tong/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/mango/tong/EurekaServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 //标记为注册中心
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
